effa07cc70413c701e3335c1555da2a5de6fab76
Sanduni Nirasha <e18329@eng.pdn.ac.lk>
2022-06-16 00:29:37 +0530

    Selenium and javascript codes for create new assignment and get marks add them to API and can use when required

--- Project/functions/index.js
+++ Project/functions/index.js
@@ -249,6 +249,150 @@ app.delete("/api/delete/:id",(req,res)=>{
     })();
 
 });
+
+//Create New Assignment in GitHub Classroom and generate the url and return it.
+app.get('/api/getUrl',(req,res)=>{
+    (async()=>{
+        try {
+            
+            //sleep function to sleep the process
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+
+            //setup the browser
+            const {Builder,By,Key, Capability, Capabilities} = require("selenium-webdriver");
+            var chrome = require('selenium-webdriver/chrome');
+            var o = new chrome.Options();
+
+            //#############################################################################################
+            //You should change this on your own
+            o.addArguments("--user-data-dir=C:/Users/HP/AppData/Local/Google/Chrome/User Data/");
+            o.addArguments("--profile-directory=Profile 3");
+            //#############################################################################################
+            
+            o.addArguments("start-minimized");
+            o.excludeSwitches("enable-automation");
+            var driver = new Builder().withCapabilities(Capabilities.chrome()).setChromeOptions(o).build();
+            driver.manage().window().minimize();
+
+
+    
+            //Go the github classroom and create new assignment
+            await driver.get("https://classroom.github.com/classrooms");
+            
+            await driver.findElement(By.xpath("//h1[text()='test-for-coding-classroom-2']")).click();
+
+            await driver.findElement(By.css("a[class='btn btn-primary right']")).click();
+
+            //page I
+            await driver.findElement(By.id("assignment_title")).sendKeys("Assignment ForMe812225");
+            await driver.findElement(By.id("assignment_form_deadline")).sendKeys("7/7/2022");
+            await driver.findElement(By.id("assignment_form_assignment_type")).sendKeys("individual");
+            await driver.findElement(By.id("assignment_form_visibility_public")).sendKeys("public");
+            await driver.findElement(By.id("new-assignment-submit")).click();
+
+            //page II
+  
+            await driver.findElement(By.css("#starter-code-repo-name")).click();
+            await driver.findElement(By.name("assignment_form[starter_code_repo_full_name]")).sendKeys("test-for-coding/template-for-java");
+            await sleep(5000);
+            await driver.findElement(By.css(".autocomplete-suggestions-list ul li strong")).click();
+            await driver.findElement(By.name("commit")).click();
+            
+            //page III
+            await driver.findElement(By.xpath("//div[@class='SelectMenu-list']//span[text()='Input/Output test']")).click();
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][name]")).sendKeys("Test1");
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][setup]")).sendKeys("javac Main.java");
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][run]")).sendKeys("java Main");
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][input]")).sendKeys("Test1");
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][output]")).sendKeys("Hello World!");
+            await driver.findElement(By.name("assignment_form[assignment_tests_attributes][][points]")).sendKeys("10");
+            await sleep(5000);
+            await driver.findElement(By.css(".js-save-test")).click();
+
+
+            //Create assignment
+            await sleep(5000);
+            await driver.findElement(By.name("commit")).click();
+    
+            //Get the url
+            let BUTTON =await driver.findElement(By.css(".input-group-button button")).getAttribute("data-clipboard-target");
+            let target = await driver.findElement(By.css(BUTTON)).getAttribute("value");
+            driver.quit();
+    
+            return res.status(200).send({status: 'Success',msg: target});
+    
+
+
+            
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send({status: 'Failed',msg: error});
+
+            
+        }
+
+    })();
+
+});
+
+
+//Get marks of a student by GitHub username
+app.get('/api/getMarks/:StudentName',(req,res)=>{
+    (async()=>{
+        try {
+            let name = req.params.StudentName;
+            
+
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            //Setup browser
+            const {Builder,By,Key, Capability, Capabilities} = require("selenium-webdriver");
+            var chrome = require('selenium-webdriver/chrome');
+            var o = new chrome.Options();
+            //#############################################################################################
+            //You should change this on your own
+            o.addArguments("--user-data-dir=C:/Users/HP/AppData/Local/Google/Chrome/User Data/");
+            o.addArguments("--profile-directory=Profile 3");
+            //#############################################################################################
+            o.addArguments("start-minimized");
+            o.excludeSwitches("enable-automation");
+            var driver = new Builder().withCapabilities(Capabilities.chrome()).setChromeOptions(o).build(); 
+            driver.manage().window().minimize();
+
+
+    
+            //Go to github classroom
+            await driver.get("https://classroom.github.com/classrooms");
+            //select the classroom
+            await driver.findElement(By.xpath("//h1[text()='test-for-coding-classroom-2']")).click();
+            
+            //select the assignment
+            await driver.findElement(By.xpath("//a[contains(.,'Assignment For1111')]")).click();
+            //search student by username
+            await driver.findElement(By.id("search-query-field")).sendKeys(name,Key.ENTER);
+            sleep(10000);
+            
+            //Get marks
+            let value=await driver.findElement(By.className("repo-detail-item Counter")).getText();
+            console.log(value);
+            driver.quit();
+            return res.status(200).send({status: 'Success',msg: value});
+          
+            
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send({status: 'Failed',msg: error});
+
+            
+        }
+
+    })();
+
+});
 //exports the api to firebase cloud functions
 exports.app = functions.https.onRequest(app);
 
